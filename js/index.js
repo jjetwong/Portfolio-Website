40a9544e61acfd7d882884529f5619f84cecd049
Jin <jjetwong@gmail.com>
2022-11-24 18:46:07 +0000

    3.0 Updated Projects and Fixed design for mobile

--- js/index.js
+++ js/index.js
@@ -4,33 +4,35 @@ const lightModeButton = document.getElementById('lightMode');
 const navOne = document.getElementById("nav1") ;
 const navTwo = document.getElementById("nav2") ;
 const navThree = document.getElementById("nav3") ;
+const artistSiteLink = document.getElementById("artistSiteLink") ;
 
 const mainTitle = document.getElementById("mainTitle");
 const aboutDesc = document.getElementById("aboutDesc");
 const portTitle = document.getElementById("portTitle");
-const contact = document.getElementById("contact");
+const artistSite = document.getElementById("artistSite")
+const contact = document.getElementById("email");
 const footer = document.getElementById("footer");
 
 let dark = false;
 
 navOne.onmouseover = function() {
-    document.getElementById("nav1").style.color = '#558564';
+    navOne.style.color = '#558564';
 }
 navTwo.onmouseover = function() {
-    document.getElementById("nav2").style.color = '#558564';
+    navTwo.style.color = '#558564';
 }
 navThree.onmouseover = function() {
-    document.getElementById("nav3").style.color = '#558564';
+    navThree.style.color = '#558564';
 }
 
 navOne.onmouseout = function() {
-    document.getElementById("nav1").style.color = '#252B2D';
+    navOne.style.color = '#252B2D';
 }
 navTwo.onmouseout = function() {
-    document.getElementById("nav2").style.color = '#252B2D';
+    navTwo.style.color = '#252B2D';
 }
 navThree.onmouseout = function() {
-    document.getElementById("nav3").style.color = '#252B2D';
+    navThree.style.color = '#252B2D';
 }
 
 function darkMode() {
@@ -45,6 +47,8 @@ function darkMode() {
         portTitle.style.color = "#E5E3DC";
         contact.style.color = "#E5E3DC";
         footer.style.color = "#E5E3DC";
+        artistSite.style.color = "#E5E3DC";
+        artistSite.style.borderBottom = "1px solid #E5E3DC";
         document.getElementById("aboutTitle").style.color = "#E5E3DC";
 
         darkModeButton.onmouseover = function() {
@@ -58,14 +62,15 @@ function darkMode() {
         navTwo.style.color = '#E5E3DC';
         navThree.style.color = '#E5E3DC';
         navOne.onmouseout = function() {
-            document.getElementById("nav1").style.color = '#E5E3DC';
+            navOne.style.color = '#E5E3DC';
         }
         navTwo.onmouseout = function() {
-            document.getElementById("nav2").style.color = '#E5E3DC';
+            navTwo.style.color = '#E5E3DC';
         }
         navThree.onmouseout = function() {
-            document.getElementById("nav3").style.color = '#E5E3DC';
+            navThree.style.color = '#E5E3DC';
         }
+
 
 
         
@@ -79,6 +84,8 @@ function darkMode() {
         portTitle.style.color = "#252B2D";
         contact.style.color = "#252B2D";
         footer.style.color = "#252B2D";
+        artistSite.style.color = "#252B2D";
+        artistSite.style.borderBottom = "1px solid #252B2D";
         document.getElementById("aboutTitle").style.color = "#252B2D";
         darkModeButton.onmouseout = function() {
             darkModeButton.style.color = '#252B2D';
@@ -87,14 +94,15 @@ function darkMode() {
         navOne.style.color = '#252B2D';
         navTwo.style.color = '#252B2D';
         navThree.style.color = '#252B2D';
+        
         navOne.onmouseout = function() {
-            document.getElementById("nav1").style.color = '#252B2D';
+            navOne.style.color = '#252B2D';
         }
         navTwo.onmouseout = function() {
-            document.getElementById("nav2").style.color = '#252B2D';
+            navTwo.style.color = '#252B2D';
         }
         navThree.onmouseout = function() {
-            document.getElementById("nav3").style.color = '#252B2D';
+            navThree.style.color = '#252B2D';
         }
     }
 }
